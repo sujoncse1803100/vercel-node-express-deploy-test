421362ab694a4c00a624b2053c688bbf37b00169
sujonali8276 <sujonali8276@gmail.com>
2024-02-13 12:25:45 +0600

    reguular update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-git command:
-
 git add .
 git commit -m "reguular update"
 git push
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-// const authRoute = require("./routes/auth");
-// const productRoute = require("./routes/product");
-// const orderRoute = require("./routes/order");
-// const promocodeRoute = require("./routes/promocode");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const promocodeRoute = require("./routes/promocode");
 
-// const MongoDB_URI = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.ohtj23d.mongodb.net/`;
+const MongoDB_URI = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.ohtj23d.mongodb.net/`;
 
-// mongoose.connect(
-//   MongoDB_URI,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log("MongoDB Conected");
-//   }
-// );
+mongoose.connect(
+  MongoDB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("MongoDB Conected");
+  }
+);
 
 app.get("/", (req, res) => {
   res.status(200).send("welcome to server");
@@ -29,10 +29,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use("/api/auth", authRoute);
-// app.use("/api/product", productRoute);
-// app.use("/api/promocode", promocodeRoute);
-// app.use("/api/order", orderRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/product", productRoute);
+app.use("/api/promocode", promocodeRoute);
+app.use("/api/order", orderRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
